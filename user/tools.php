c66efe61d0c46293f27465624fd4f78f62a7e950
Lekhraj KR <mailtoahuman@gmail.com>
2015-09-23 12:08:19 +0530

    Create tools.php

---
+++ user/tools.php
@@ -0,0 +1,19 @@
+<?php include '../backend/authentication.php' ?>     
+<?php include '../backend/session-check.php' ?>
+<?php include '../backend/user-detail.php' ?>
+<html>
+<head>
+<title><?php echo $row['ca_cname']; ?> ~ Tools</title>
+<link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+<?php include '../contain/nav-tools.php' ?>
+<div class="container">
+<div class="l-box">
+<div class="notify">
+<h3></h3>
+</div>
+</div>
+</div>
+</body>
+</html>
