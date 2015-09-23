b3abed9b329c25a75494e17d46aa2df2e8f9ffc6
Lekhraj KR <mailtoahuman@gmail.com>
2015-09-23 11:55:51 +0530

    Create nav-storage.php

---
+++ contain/nav-storage.php
@@ -0,0 +1,10 @@
+
+<div class="bar-hold ">
+<a href="home.php" class="brand "><b>Kodayickal Internet</b></a>
+<table class="float-right "><tr>
+<td><a class="button" href="home.php?status=tab">Home</a></td>
+<td><a class="bactive" href="storage.php?status=tab">Storage</a></td>
+<td><a class="button" href="settings.php?status=tab">Settings</a></td>
+<td><a class="logout" href="logout.php?status=tab">Logout</a></td></tr>
+</table>
+</div>
